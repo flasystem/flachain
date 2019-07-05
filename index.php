671fde3f2b8c9d1ba45f4d734f894a437974f9bf
ljh <ljh@flasystem.com>
2019-07-05 13:54:54 +0900

    html - php

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
 	<div id="wrap">
 		<header>
 			<h1 class="logo">
-				<img src="/img/flachain.png" alt="flachain">
+				<img src="img/flachain.png" alt="flachain">
 			</h1>
 		</header>
+		<?php include('form_process');?>
 		<div id="container">
 			<div class="clear">
 				<label>
@@ -21,7 +22,25 @@
 				<label>
 					<input type="radio" name="" title=""> 신청서 받기
 				</label>
-				<div class="enquire"></div>
+				<div class="enquire">
+					<form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+						<fieldset>
+							<table>
+								<tr>
+									<th>문의내용</th>
+								</tr>
+							</table>
+							<table>
+								<tr>
+									<th>성함</th>
+									<td>
+										<input type="text" name="name" value="<?=$name?>">
+									</td>
+								</tr>
+							</table>
+						</fieldset>
+					</form>
+				</div>
 				<div class="application_form"></div>
 			</div>
 		</div>
